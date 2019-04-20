47270b45e2e8fcbc18be8d76c3470166b52a6c0a
eko <energeticbinge@gmail.com>
2019-04-20 17:54:58 +0600

    exercises

---
+++ parts6-8/eighth_part/magic.py
@@ -0,0 +1,11 @@
+def magicians(list_m):
+	for mag in list_m:
+		print(mag)
+
+def make_great(list_m):
+	for mag in list_m:
+		print(mag + ' Great')
+	
+names = ['John','Lenni','Lina']
+magicians(names)
+make_great(names)
